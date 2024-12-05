0bb58b5c61a5e1ae846fa6a812b19b80662a6117
codenamecriz <codenamecriz@gmail.com>
2024-12-05 10:41:14 +0800

    Added Property Details Component

--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -1,3 +1,4 @@
+import PropertyDetails from "@/components/Properties/PropertyDetails";
 import PropertyHeaderImage from "@/components/Properties/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
@@ -23,6 +24,14 @@ const PropertyPage = async ({ params }) => {
           >
             <FaArrowLeft className="mr-2" /> Back to Properties
           </Link>
+        </div>
+      </section>
+      {/* <!-- Property Info --> */}
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
         </div>
       </section>
     </>
