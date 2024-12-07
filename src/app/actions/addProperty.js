5a40d12c13155306ba9b127d0e3ddf1c8428b542
codenamecriz <codenamecriz@gmail.com>
2024-12-07 10:27:40 +0800

    Added Submit Property to database

--- src/app/actions/addProperty.js
+++ src/app/actions/addProperty.js
@@ -1,6 +1,20 @@
 "use server";
 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 async function addProperty(formData) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required.");
+  }
+
+  const { userId } = sessionUser;
+
   // Access all values from amenities and images
   const amenities = formData.getAll("amenities");
   const images = formData
@@ -9,6 +23,7 @@ async function addProperty(formData) {
     .map((image) => image.name);
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -36,6 +51,13 @@ async function addProperty(formData) {
   };
 
   console.log(propertyData);
+
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
