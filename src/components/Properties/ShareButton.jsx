f702f00b350054445e5e26e89c92e629afefc6ca
codenamecriz <codenamecriz@gmail.com>
2025-01-06 08:54:54 +0800

    Added Share Buttons

--- src/components/Properties/ShareButton.jsx
+++ src/components/Properties/ShareButton.jsx
@@ -1,11 +1,58 @@
+"use client";
 import React from "react";
-import { FaShare } from "react-icons/fa";
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  ViberIcon,
+  ViberShareButton,
+} from "react-share";
 
 const ShareButton = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="fas fa-share mr-2" /> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share This Property:
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        {/* Facebook */}
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type.replace(/\s/g, "")}ForRent`}
+        >
+          {console.log(`FB URL: ${shareUrl}`)}
+          <FacebookIcon size={40} round={true}></FacebookIcon>
+        </FacebookShareButton>
+
+        {/* Twitter */}
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`${property.type.replace(/\s/g, "")}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true}></TwitterIcon>
+        </TwitterShareButton>
+
+        {/* Viber */}
+        <ViberShareButton url={shareUrl} title={property.name} separator=":: ">
+          <ViberIcon size={40} round={true}></ViberIcon>
+        </ViberShareButton>
+
+        {/* Email */}
+        <EmailShareButton
+          url={shareUrl}
+          subject={property.name}
+          body={`Check out this property: ${shareUrl}`}
+        >
+          <EmailIcon size={40} round={true}></EmailIcon>
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 
