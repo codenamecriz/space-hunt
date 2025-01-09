309f3003c179a0b75228e9a020c399fc9ae60af4
codenamecriz <codenamecriz@gmail.com>
2025-01-09 14:31:48 +0800

    Added Photoswipe Lightbox for Property Images

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "@/assets/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
